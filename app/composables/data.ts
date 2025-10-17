310f74d1e78d871fb0f2323d5865328fc2f93e01
deepak.kumar <deepak.kumar@suretekinfosoft.com>
2025-10-17 11:13:00 +0530

    markup points are done

--- app/composables/data.ts
+++ app/composables/data.ts
@@ -15,6 +15,30 @@ export const keyPoints = [
 ];
 
 export const faqs = [
+    {
+        "question": "How long does it take to receive my order?",
+        "answer": "With Standard Shipping, your order should arrive in 3-5 days. However, we’ve noticed many families want it sooner, so there’s an option for expedited shipping.  This means it should arrive in 2 days."
+    },
+    {
+        "question": "Where is YOMZ manufactured?",
+        "answer": "We’re proud to manufacture our tasty gummies in the USA. We use premium ingredients in our state-of-the-art facility, which is FDA-registered and GMP-certified. Then YOMZ is 3rd-party tested to ensure top-shelf quality."
+    },
+    {
+        "question": "What if YOMZ is not for me - is there a refund?",
+        "answer": "Your order today is protected by our 90-Day Picky Momz 200% Happiness Guarantee. We’re so confident in the real food-based nutrition inside every YOMZ gummy that we’re sure you won’t need it. But you deserve ZERO stress when fighting for your family’s health, so here’s how it works: If you don’t see a difference in your family’s focus, digestion, energy, even your kid’s tantrum tempo… If they taste it and give you the dreaded thumbs down… Or if YOMZ doesn’t feel like the win you needed… You get back double what you paid."
+    },
+    {
+        "question": "Does YOMZ really taste that good?",
+        "answer": "Yes, it does. In our experience, no matter how healthy something is, it doesn’t matter if it doesn’t taste good - because who’ll want to eat it every day. So we craft YOMZ flavor-first. Besides, with a name like YOMZ, it has to taste phenomenal. And it does. "
+    },
+    {
+        "question": "Is it safe for kids?",
+        "answer": "Yes. Family is one of our 3 core values, so it has to be safe for ages 2 to 102. That’s why YOMZ has no artificial dyes, flavors, or other junk. It’s free of GMOs, gluten, and dairy plus vegan-friendly."
+    },
+    {
+        "question": "How many bags do I need for my family?",
+        "answer": "One bag of YOMZ is filled with 28 packs. Each pack equals a daily serving for 1 person. So 1 family member = 1 bag of nutrition for 28 days. So how many bags you need depends on how many family members you want to help."
+    }    
     // {
     //     "question": "Do I really need to continue taking YOMZ after a couple of months or so?",
     //     "answer": "No, you don’t. Our mission is to end Hidden Hunger for 100 million people by 2030. If it happens because families take their YOMZ daily for the next 3 years, then great. But if it happens because by taking YOMZ, you and your family were inspired to transform your lives by overhauling your diet, exercise routine, nutritional awareness, etc., then that’s great, too. We just want to help you however we can."
@@ -27,26 +51,14 @@ export const faqs = [
     //     "question": "How about helping with brain function, immunity, even heart health?",
     //     "answer": "Yes. Again, everything starts with improved gut health. In fact, there’s a direct link between gut health and brain function. Similarly, better gut health is critical for a more robust immune system and a stronger heart."
     // },
-    {
-        "question": "Is it safe for kids?",
-        "answer": "Yes. Family is one of our 3 core values, so it has to be safe for ages 2 to 102. That’s why YOMZ has no artificial dyes, flavors, or other junk. It’s free of GMOs, gluten, and dairy plus vegan-friendly."
-    },
     // {
     //     "question": "Will YOMZ help me lose weight?",
     //     "answer": "Let’s be clear, YOMZ is not a weight loss gummy. Even if you take 2-3 servings per day. However, if YOMZ helps you and your family be more active… If it causes you to reduce how much processed foods you eat… Or if by helping improve your immune system or kids’ growth and development, YOMZ helps you become stronger… then who knows what other results are possible? 😉"
     // },
-    {
-        "question": "What if I don’t notice a difference — is there a guarantee?",
-        "answer": "It would be a shame. That’s why we have the most ridiculously iron-clad guarantee you’ll find anywhere. If you don’t absolutely love YOMZ, simply reach out to us within 90 days and we’ll cheerfully refund double what you paid. It’s the YOMZ 90-Day Picky Momz 200% Happiness Guarantee. We take on all the risk so you can try it with total peace of mind. The worst that’ll happen is you double your investment in 90 days. Not even Wall St. can guarantee that."
-    },
     // {
     //     "question": "Will I have to change my family’s diet or meals or snack routine?",
     //     "answer": "Not at all. It’s really up to you and your family. If you don’t change anything, you can still rest easier knowing you’re leveling up your nutrition. That’s a win right there. But there’s a very good chance that the mere daily routine of filling those nutrition gaps with YOMZ will lead to eating better. Once you start filling your nutrition needs, cravings for junk vanish. So play it by ear and your heart will know if change is good."
     // },
-    {
-        "question": "How many bags do I need for my family?",
-        "answer": "One bag of YOMZ is filled with 28 packs. Each pack equals a daily serving for 1 person. So 1 family member = 1 bag of nutrition for 28 days. So how bags you need depends on how many family members you want to help."
-    },
     // {
     //     "question": "Why is it priced lower than your competitors?",
     //     "answer": "YOMZ has no competition, first and foremost. It’s a unicorn. No other gummy delivers amazing taste that’s also packed with antioxidants, vitamins, micronutrients, and 5g of fiber. As for price, there is no middleman. We’re making this offer directly to you. Because we don’t have to pay a middleman to offer it or sell it to you, we’re passing the savings on to you. On top of that, our mission is to end Hidden Hunger for 100 million people by 2030. There’s no way we could do that if we jacked up the price. This philosophy will never change. It’s the foundation of YOMZ."
